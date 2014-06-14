609fcb5afb98f40a6ba900456c3f30b2baa8a2f3
Szabolcs Szekely <makrela@gmail.com>
2014-06-14 21:32:20 +0200

    The first version of the ninth exercise.

---
+++ ex9.py
@@ -0,0 +1,15 @@
+# This is the ninth exercise.
+# Date: 2014-06-14
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we wanr, or 5, or 6.
+"""
