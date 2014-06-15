421cb3adba28d4d846fd5763b741d59bd732748e
Szabolcs Szekely <makrela@gmail.com>
2014-06-16 01:00:11 +0200

    The initial version of the eleventh exercise.

---
+++ ex11.py
@@ -0,0 +1,12 @@
+# This is the eleventh exercise
+# Date: 2014-06-16
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you are %s old, %s tall and %s heavy." % (
+	age, height, weight)
