1919c82e2bbb914dc5ef4c432c36f5aec48bba2b
Szabolcs Szekely <makrela@gmail.com>
2014-06-16 01:43:57 +0200

    The initial version of the twelveth exercise.

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# This is the twelth exercise.
+# Date: 2014-06-16
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So you are %s old, %s tall and %s heavy." % (
+	age, height, weight)
