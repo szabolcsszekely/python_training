d44cd24557d787df484056090a0dd366fd56aa45
Szabolcs Szekely <makrela@gmail.com>
2014-06-16 23:40:52 +0200

    The initial version of the thirteenth exercise.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+# This is the thirteenth exercise.
+# Date: 2014-06-16
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
