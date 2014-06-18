71740ef7a4f293b4c1b0c85cc42dfaa1ef7962ce
Szabolcs Szekely <makrela@gmail.com>
2014-06-18 08:03:35 +0200

    The initial version of the sixteenth exercise.

---
+++ ex16.py
@@ -0,0 +1,36 @@
+# This is the sisteenth exercise.
+# Date: 2014-06-18
+
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Truncatung the file. Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
