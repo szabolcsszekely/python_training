e5cf7c7dad558214a2ccbd069e257407b11e8eed
Szabolcs Szekely <makrela@gmail.com>
2014-06-19 21:42:54 +0200

    The initial version of th eighteenth exercise.

---
+++ ex18.py
@@ -0,0 +1,20 @@
+# This is the eigthteenth exercise.
+# Date: 2014-06-19
+
+def print_two(*args):
+    arg1, arg2 = args
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+def print_two_again(arg1, arg2):
+    print "arg1: %s, arg2: %s" % (arg1, arg2)
+
+def print_one(arg1):
+    print "arg1: %s" % arg1
+
+def print_none():
+    print "I got nothing."
+
+print_two("Zed", "Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
