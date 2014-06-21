214d7aaa4408c8b7fee13d2794cc1d6d809ce407
Szabolcs Szekely <makrela@gmail.com>
2014-06-21 22:11:06 +0200

    First version of the twenty-ninth exercise.

---
+++ ex29.py
@@ -0,0 +1,31 @@
+# This is the twenty-ninth exercise.
+# Date: 2014-06-21
+
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats!"
+
+if people > cats:
+    print "Not many cats!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The worlds is dry!"
+
+
+dogs += 5
+
+if people >= dogs:
+    print "People are greater than or equal to dogs."
+
+if people <= dogs:
+    print "People are less than or equal to dogs."
+
+if people == dogs:
+    print "People are dogs."
