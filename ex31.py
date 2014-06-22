00d68748485217b3fe8af7bc7468e02461e4183d
Szabolcs Szekely <makrela@gmail.com>
2014-06-22 11:36:21 +0200

    First version of the thirty-first exercise.

---
+++ ex31.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is the thirty-first exercise.
+# Date: 2014-06-22
+
+print "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+
+door = raw_input("> ")
+
+if door == "1":
+    print "There's a giant bear here eating a cheese cake. What do you do?"
+    print "1. Take the cake."
+    print "2. Scream at the bear."
+
+    bear = raw_input("> ")
+
+    if bear == "1":
+        print "The bear eats your face off."
+    elif bear == "2":
+        print "The bear eats your legs off."
+    else:
+        print "Well doing %s is probably better. Bear runs away." % bear
+
+elif door == "2":
+    print "You stare into the endless abyss at Cthulhu's retina."
+    print "1. Blueberries."
+    print "2. Yellow jacket clothespins."
+    print "3. Understanding revolvers yelling melodies."
+
+    insanity = raw_input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print "Your body survives powered by a mind of jello."
+    else:
+        print "The insanity rots your eyes into a pool of muck."
+
+else:
+    print "You stumble around and fall on a knife and die."
