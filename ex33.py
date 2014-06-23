b2d7e9d545722d167b401d2c9f46eeea91c58650
Szabolcs Szekely <makrela@gmail.com>
2014-06-23 06:41:33 +0200

    Added more elaborate loops.

--- ex33.py
+++ ex33.py
@@ -17,3 +17,27 @@ print "The numbers: "
 
 for num in numbers:
     print num
+
+def loop_test(n):
+    """Adds numbers to a list until n."""
+    i = 0
+    container = []
+    while i < n:
+        # print "Adding %d to the list." % i
+        container.append(i)
+        i += 1
+    return container
+
+counter = int(raw_input("How many numbers do you want to put in the list? "))
+increment = int(raw_input("In what increments do you want to put numbers in the list? "))
+
+print "The list: ", loop_test(counter)
+
+def smart_loop(end, inc):
+    """Adds numbers to a list until end with inc increments."""
+    container = []
+    for i in range(0,end,inc):
+        container.append(i+1)
+    return container
+
+print "The smart list: ", smart_loop(counter, increment)
